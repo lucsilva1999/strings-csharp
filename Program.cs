8deb2112065527f5e5ba3a23f2f4cd695d2d4713
Lucas Silva <lucsilva1999@gmail.com>
2020-11-26 14:20:37 -0300

    Testando array de strings com seleção aleatória.
    Testando array de strings com seleção aleatória da posição utilizando o Random e outros modos com utilização das variáveis.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           string[] teste = new string[] {"1", "2", "3", "4", "5" };
+           string selecionaPosicao = teste[1];
+
+           Random rand = new Random();
+
+           int posicaoAleatoria = rand.Next(teste.Length);
+
+           string aleatorio = posicaoAleatoria.ToString();
+
+           MeuMetodo(teste[1]);
+           MeuMetodo(selecionaPosicao);
+           MeuMetodo(aleatorio);
+
+        }
+
+        static void MeuMetodo(string parametro)
+        {
+            Console.WriteLine(parametro);
+        }
+    }
+}
